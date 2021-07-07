d9d90ad16ba262ae2b32df407616e60537b8a214
Coachbre <brekonan1@gmail.com>
2021-07-07 12:23:40 -0400

    models and most repo files have been created

---
+++ Streamish/Repositories/BaseRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace Streamish.Repositories
+{
+    public abstract class BaseRepository
+    // 'abstract' indicates that our BaseRepository class cannot be directly instantiated, but can ONLY be used by inheritance.
+    {
+        private readonly string _connectionString;
+
+        public BaseRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        protected SqlConnection Connection
+        //We mark the Connection property as protected to make it available to child classes, but inaccessible to any other code.
+        {
+            get
+            {
+                return new SqlConnection(_connectionString);
+            }
+        }
+    }
+}
