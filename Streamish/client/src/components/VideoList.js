f55aeba6901c8f2ce323e546861ffa1a65f4946a
Coachbre <brekonan1@gmail.com>
2021-07-12 14:44:28 -0400

    added search feature

--- Streamish/client/src/components/VideoList.js
+++ Streamish/client/src/components/VideoList.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Video from './Video';
 import { getAllVideos } from "../modules/videoManager";
 
 const VideoList = () => {
@@ -13,15 +14,18 @@ const VideoList = () => {
   }, []);
 
   return (
-    <div>
-      {videos.map(v => 
-        <div>{v.title}</div>
-      )}
+    <div className="container">
+      <div className="row justify-content-center">
+        {videos.map((video) => (
+          <Video video={video} key={video.id} />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default VideoList;
+
 
 //When the component loads, it will call the getAllVideos function,
 // set the state of the videos array and re-render to display a list of video titles.
