141ce8950d91af2cc61ea3a4f2be294df2925dea
Coachbre <brekonan1@gmail.com>
2021-07-12 15:01:10 -0400

    added react application views

---
+++ Streamish/client/src/components/ApplicationViews.js
@@ -0,0 +1,27 @@
+// controls which view is shown for each route (url)
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+//The Switch component is going to look at the url and render the first route that is a match.
+import VideoList from "./VideoList";
+import VideoForm from "./VideoForm";
+
+const ApplicationViews = () => {
+    // If a url matches the value of the path attribute, the children of that route will render
+    //'exact' specifies the URL must match completely
+    //':id' is a route param
+  return (
+    <Switch>
+      <Route path="/" exact>
+        <VideoList />
+      </Route>
+
+      <Route path="/videos/add">
+        <VideoForm />
+      </Route>
+
+      <Route path="/videos/:id">{/* TODO: Video Details Component */}</Route>
+    </Switch>
+  );
+};
+
+export default ApplicationViews;
